5fb907b157da9f327a6f9564eff4dcc5055cab71
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-06-25 08:37:27 -0500

    setup inicial

---
+++ ejercicio/src/main/java/main/ClsMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ronnycajas
+ */
+public class ClsMain {
+    
+
+        public static void main(String[] args) {
+            
+        String nombre;
+        String edad;
+        
+        System.out.printf("Agenda en python ;v");
+        
+
+        
+        
+        
+    }
+    
+}
